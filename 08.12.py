b3a78639e7321f84668211abd67c3b65f320c67a
Sophie Eichhorn <s.eichhorn3004@gmail.com>
2024-12-08 11:47:01 +0100

    Add solution to day 8 part 2

--- 08.12.py
+++ 08.12.py
@@ -18,7 +18,7 @@ def calc_distance(point1: tuple, point2: tuple) -> tuple:
     return point1[0] - point2[0], point1[1] - point2[1]
 
 
-def is_in_grid(grid: list, point: tuple) -> bool:
+def is_on_grid(grid: list, point: tuple) -> bool:
     return 0 <= point[0] < len(grid) and 0 <= point[1] < len(grid[0])
 
 
@@ -29,6 +29,7 @@ with open('files/day8input.txt') as file:
 antennas = load_antenna_locations(file)
 print(f'Antennas: {antennas}')
 
+# part 1
 antinodes = []
 for signal_name in antennas:
     location_tuples = [(x, y) for x, y in product(antennas[signal_name], repeat=2) if x != y]
@@ -36,7 +37,27 @@ for signal_name in antennas:
     for location_tuple in location_tuples:
         antenna_distance = calc_distance(location_tuple[0], location_tuple[1])
         antinode_location = (location_tuple[0][0]+antenna_distance[0], location_tuple[0][1]+antenna_distance[1])
-        if is_in_grid(file, antinode_location):
+        if is_on_grid(file, antinode_location):
             antinodes.append(antinode_location)
 
 print(f'Antenna locations: {len(set(antinodes))}')
+
+# part 2
+antinodes = []
+for signal_name in antennas:
+    location_tuples = [(x, y) for x, y in product(antennas[signal_name], repeat=2) if x != y]
+
+    for location_tuple in location_tuples:
+        antenna_distance = calc_distance(location_tuple[0], location_tuple[1])
+        reference_point = location_tuple[0]
+        antinodes.append(reference_point)  # add self to the list
+
+        on_grid = True
+        while on_grid:
+            reference_point = (reference_point[0]+antenna_distance[0], reference_point[1]+antenna_distance[1])
+            if is_on_grid(file, reference_point):
+                antinodes.append(reference_point)
+            else:
+                on_grid = False
+
+print(f'Antenna locations with resonant harmonics : {len(set(antinodes))}')
