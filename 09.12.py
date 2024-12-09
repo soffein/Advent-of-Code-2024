84a40e1a59dcc1819d4b9bafd88d2839473a938e
Sophie Eichhorn <s.eichhorn3004@gmail.com>
2024-12-09 10:01:28 +0100

    Add defragmentation

--- 09.12.py
+++ 09.12.py
@@ -1,4 +1,6 @@
 SPACE = '.'
+file_content = []
+defragmented_content = []
 
 
 def is_even(number: int) -> bool:
@@ -6,8 +8,7 @@ def is_even(number: int) -> bool:
 
 
 def generate_file_content() -> list:
-    global instruction
-    file_content = []
+    global instruction, file_content
     file_index = 0
 
     for i, char in enumerate(instruction):
@@ -21,9 +22,37 @@ def generate_file_content() -> list:
 
     return file_content
 
+
+def find_index_of_last_number(stop_index) -> int:
+    index = 0
+    for i in range(len(defragmented_content)-1, stop_index, -1):
+        if isinstance(defragmented_content[i], int):
+            index = i
+            break
+    return index
+
+
+def switch_position() -> bool:
+    switched = False
+    first_space_index = defragmented_content.index(SPACE)
+    last_number_index = find_index_of_last_number(first_space_index)
+    if first_space_index < last_number_index:
+        defragmented_content[first_space_index], defragmented_content[last_number_index] = defragmented_content[last_number_index], defragmented_content[first_space_index]
+        switched = True
+    return switched
+
+
+def defrag():
+    global defragmented_content
+    defragmented_content = list(file_content)
+    while switch_position():
+        pass
+
+
 with open('files/day9input.txt') as input_file:
 
     instruction = input_file.readline()
 
-generated_content = generate_file_content()
-print(generated_content)
+generate_file_content()
+defrag()
+print(defragmented_content)
