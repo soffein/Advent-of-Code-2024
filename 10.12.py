be4aba1378e90a3b3b94a7a7a2fc2e70490a2454
Sophie Eichhorn <s.eichhorn3004@gmail.com>
2024-12-10 10:55:07 +0100

    Find start positions

--- 10.12.py
+++ 10.12.py
@@ -1,6 +1,19 @@
+
+def find_starts():
+    global grid, starting_positions
+    for x, row in enumerate(grid):
+        for y, cell in enumerate(row):
+            if cell == 0:
+                starting_positions.append((x, y))
+
+
 with open('files/day10input.txt') as input_file:
 
-    grid = [list(line.strip()) for line in input_file]
+    grid = [[int(char) for char in line.strip()] for line in input_file]
 
-print(grid)
+starting_positions = []
+find_starts()
+
+print(f'Grid: {grid}')
+print(f'Starting_positions: {starting_positions}')
 print(f'Grid has {len(grid)} rows and {len(grid[0])} columns')
